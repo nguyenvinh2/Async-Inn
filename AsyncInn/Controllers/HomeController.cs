0cdc342d3cc8dce7e0347b61c013b51197b2b747
nguye <nguyenv2@outlook.com>
2018-10-25 10:24:04 -0700

    can't intialize database

---
+++ AsyncInn/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncInn.Controllers
+{
+  public class HomeController: Controller
+  {
+    [HttpGet]
+    public ViewResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AsyncInn.Models;
+
+namespace AsyncInn.Data
+{
+  public class AsyncInnDbContext : DbContext
+  {
+    public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<RoomAmenity>().HasKey(
+				ra => new { ra.AmenityID, ra.RoomID }
+				);
+		}
+
+		public DbSet<Room> Rooms { get; set; }
+		public DbSet<HotelRoom> HotelRooms { get; set; }
+		public DbSet<Hotel> Hotels { get; set; }
+		public DbSet<RoomAmenity> RoomAmenities { get; set; }
+    public DbSet<Amenity> Amenities { get; set; }
+  }
+}
