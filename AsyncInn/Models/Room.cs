e64609b5f0b50931e68fa8984740e638c324d2b5
nguye <nguyenv2@outlook.com>
2018-10-25 12:27:32 -0700

    all controllers added

--- AsyncInn/Models/Room.cs
+++ AsyncInn/Models/Room.cs
@@ -12,8 +12,6 @@ namespace AsyncInn.Models
     public int ID { get; set; }
     public string Name { get; set; }
     public Layout Layout { get; set; }
-    public int Age { get; set; }
-
     public ICollection<RoomAmenity> RoomAmenities { get; set; }
     public ICollection<HotelRoom> HotelRooms { get; set; }
   }
