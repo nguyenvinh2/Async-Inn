c96bba0bec5f1999575a48882a8d8f85f07a750d
nguye <nguyenv2@outlook.com>
2018-10-29 14:35:17 -0700

    crafted interface and services

---
+++ AsyncInn/Models/Interfaces/IHotels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+  public interface IHotels
+  {
+    // Creating
+    Task CreateHotels(Hotel Hotel);
+
+    // updating
+    Task UpdateHotels(Hotel Hotel);
+
+    //deleting
+    Task DeleteHotels(int id);
+
+    // Read
+    Task<List<Hotel>> GetHotels();
+
+    Task<Hotel> GetHotel(int? id);
+  }
+}
---
+++ AsyncInn/Models/Services/RoomServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncInn.Models.Services
+{
+  public class RoomServices : IRooms
+  {
+    private AsyncInnDbContext _context;
+    public RoomServices(AsyncInnDbContext context)
+    {
+      _context = context;
+    }
+    public async Task CreateRooms(Room Room)
+    {
+      _context.Rooms.Add(Room);
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteRooms(int id)
+    {
+      Room Room = await GetRoom(id);
+      _context.Rooms.Remove(Room);
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task<Room> GetRoom(int? id)
+    {
+      return await _context.Rooms.FirstOrDefaultAsync(x => x.ID == id);
+    }
+
+    public async Task<List<Room>> GetRooms()
+    {
+      return await _context.Rooms.ToListAsync();
+    }
+
+    public async Task UpdateRooms(Room Room)
+    {
+      _context.Rooms.Update(Room);
+      await _context.SaveChangesAsync();
+    }
+  }
+}
